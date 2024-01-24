de9c597e75e8804c7fff53e4fb2d242221968778
jaquejak <jaqueline.44rodrigues@gmail.com>
2024-01-24 10:30:52 -0300

    att

---
+++ 2DES/BCD/aula01/script.sql
@@ -0,0 +1,11 @@
+-- Criar um bancon de dados
+create database lojinha;
+
+-- Listar todos os bancos de dados
+show databases;
+
+-- Acessar um banco de dados
+use lojinha;
+
+-- Excluir um banco de dados
+drop database lojinha;
